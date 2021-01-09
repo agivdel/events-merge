e86d9f105cf0de4b3cf2a252e1d44ddfbc062ebb
agivdel <agph@yandex.ru>
2021-01-09 14:15:22 +0300

    clear Main class

--- src/main/java/agivdel/eventsMerge/Main.java
+++ src/main/java/agivdel/eventsMerge/Main.java
@@ -1,11 +1,5 @@
 package agivdel.eventsMerge;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import static java.time.LocalTime.*;
-import java.util.*;
-
 /**
  * Есть расписание. Каждое событие в расписании имеет время начала и время конца. Пересечения событий допускаются.
  * Нужно найти пересекающиеся события и слить их в единые события.
@@ -19,33 +13,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        new Main().run();
-    }
-
-    public void run() {
-        Set<Event> eventSet = getEventSet(
-                of(15, 0), of(15, 30),
-                of(15, 20), of(16, 30),
-                of(17, 40), of(17, 45),
-                of(17, 20), of(18, 0),
-                of(19, 20));
-
-        System.out.println("before events merge: ");
-        eventSet.forEach(System.out::println);
-        System.out.println(eventSet.size());
-        Set<Event> newSet = new Merger().setToMerge(eventSet);
-        System.out.println("after merge: ");
-        newSet.forEach(System.out::println);
-
-    }
-
-    private static Set<Event> getEventSet(LocalTime... localTimes) {
-        Set<Event> eventSet = new TreeSet<>();
-        Event event;
-        for (int i = 0; i < localTimes.length - 1; i += 2) {
-            event = new Event(localTimes[i], localTimes[i + 1]);
-            eventSet.add(event);
-        }
-        return eventSet;
     }
 }
